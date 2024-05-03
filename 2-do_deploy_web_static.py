c1a1e0f975414a8addd86f3bb47446815f3d8cab
abdel nissoul <abdelnissoul205@gmail.com>
2024-05-03 11:03:43 -0700

    ls

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
 """
-Fabric script for distributing an archive to web servers.
+making a fabric based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
+
 
 from fabric.api import put, run, env
 from os.path import exists
-
 env.hosts = ['54.237.23.243', '3.80.18.18']
 
+
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers.
-
-    Args:
-        archive_path (str): Path to the archive to be deployed.
-
-    Returns:
-        bool: True if all operations have been done correctly, False otherwise.
-    """
-    if not exists(archive_path):
+    """showing the archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        file_name = archive_path.split("/")[-1]
-        no_ext = file_name.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-
         put(archive_path, '/tmp/')
-        run('mkdir -p {0}{1}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{0} -C {1}{2}/'.format(file_name, path, no_ext))
-        run('rm /tmp/{0}'.format(file_name))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {0}{1}/web_static'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {0}{1}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
-        print("Error deploying archive:", e)
+    except:
         return False
